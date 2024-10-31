8cc4b3f43e48ebdff42a5791da30cc53d19f1514
odinstudent <odinstudent@Xubuntu.myguest.virtualbox.org>
2024-10-31 17:04:02 -0500

    initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <>
+    <p>prueba inicial para un nuevo proyecto</p>
+    </>
+  );
+}
+
+export default App;
