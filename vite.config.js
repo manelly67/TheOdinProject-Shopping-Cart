a9bb77da642f8c7fa89d26dece6da3f62ef7e267
manellylopez <manellylopezqqq@gmail.com>
2025-06-17 15:00:11 -0500

    add vite-plugin-compression and configure

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      algorithm: "brotliCompress",
+      ext: ".br",
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
